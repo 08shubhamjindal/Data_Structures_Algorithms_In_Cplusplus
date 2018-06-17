bc87e2737bfce441b19e9e78b7f443c023a1d538
08shubhamjindal <shubhamjindal.cse19@jecrc.ac.in>
2018-06-17 22:50:05 +0530

    Play with string with some condition

--- Play-With-String.cpp
+++ Play-With-String.cpp
@@ -1,20 +1,20 @@
-You are given a string S followed by two integers N1 and N2. You need to change the case of character at those two indices as specified by the two integers.
+// You are given a string S followed by two integers N1 and N2. You need to change the case of character at those two indices as specified by the two integers.
 
-Input Format
+// Input Format
 
-First line contains the string S Second line contains two integers separated by single space
+// First line contains the string S Second line contains two integers separated by single space
 
-Output Format
+// Output Format
 
-It should print the string with case changed at those locations
-SAMPLE INPUT
+// It should print the string with case changed at those locations
+// SAMPLE INPUT
 
-Jamia Hamdard
-2 9
+// Jamia Hamdard
+// 2 9
 
-SAMPLE OUTPUT
+// SAMPLE OUTPUT
 
-JAmia HaMdard
+// JAmia HaMdard
 
 
 
