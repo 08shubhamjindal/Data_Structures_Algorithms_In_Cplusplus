3ab139b577c1f2550d92011c2da1f5bb747e3207
08shubhamjindal <shubhamjindal.cse19@jecrc.ac.in>
2018-06-19 01:07:14 +0530

    Swapping of 0 and 1 bit in String -Codechef

--- Binary-Search-Tree-Using-STL.cpp
+++ Binary-Search-Tree-Using-STL.cpp
@@ -1,32 +1,54 @@
-Given an array A of N integers, classify it as being Good Bad or Average. It is called Good, if it contains exactly X distinct integers, Bad if it contains less than X distinct integers and Average if it contains more than X distinct integers.
+// Given an array A of N integers, classify it as being Good Bad or Average. It is called Good, if it contains exactly X distinct integers, Bad if it contains less than X distinct integers and Average if it contains more than X distinct integers.
 
-Input format:
-First line consists of a single integer T denoting the number of test cases.
-First line of each test case consists of two space separated integers denoting N and X.
-Second line of each test case consists of N space separated integers denoting the array elements.
+// Input format:
+// First line consists of a single integer T denoting the number of test cases.
+// First line of each test case consists of two space separated integers denoting N and X.
+// Second line of each test case consists of N space separated integers denoting the array elements.
 
-Output format:
-Print the required answer for each test case on a new line.
+// Output format:
+// Print the required answer for each test case on a new line.
 
-Constraints:
+// Constraints:
 
 
-SAMPLE INPUT
+// SAMPLE INPUT
 
-4
-4 1
-1 4 2 5
-4 2
-4 2 1 5
-4 3
-5 2 4 1
-4 4
-1 2 4 5
+// 4
+// 4 1
+// 1 4 2 5
+// 4 2
+// 4 2 1 5
+// 4 3
+// 5 2 4 1
+// 4 4
+// 1 2 4 5
 
-SAMPLE OUTPUT
+// SAMPLE OUTPUT
 
-Average
-Average
-Average
-Good
+// Average
+// Average
+// Average
+// Good
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+long long t;
+cin>>t;
+for(int a=0;a<t;a++){
+long long n,x;
+cin>>n;
+cin>>x;
+set<long long> s;
+int b;
+for( long long i=0;i<n;i++)
+{
+cin>>b;
+s.insert(b);
+}
+if(s.size()==x){cout<<"Good"<<endl;}
+if(s.size()<x){cout<<"Bad"<<endl;}
+if(s.size()>x){cout<<"Average"<<endl;
+}
+}
 
+}
---
+++ Swap-with-0-1-String.cpp
@@ -0,0 +1,79 @@
+Chef has a number D containing only digits 0's and 1's. He wants to make the number to have all the digits same. For that, he will change exactly one digit, i.e. from 0 to 1 or from 1 to 0. If it is possible to make all digits equal (either all 0's or all 1's) by flipping exactly 1 digit then output "Yes", else print "No" (quotes for clarity)
+Input
+
+The first line will contain an integer T representing the number of test cases.
+
+Each test case contain a number made of only digits 1's and 0's on newline
+Output
+
+Print T lines with a "Yes" or a "No", depending on whether its possible to make it all 0s or 1s or not. 
+Constraints
+Subtask #1: (40 points)
+
+    1 ≤ T ≤ 50
+    1 ≤ Length of the number D ≤ 50
+
+
+Subtask #2: (60 points)
+
+    1 ≤ T ≤ 10
+    1 ≤ Length of the number D ≤ 105
+
+Example
+
+Input:
+2
+101
+11
+Output:
+Yes
+No
+
+Explanation
+
+Example case 1. In 101, the 0 can be flipped to make it all 1..
+
+Example case 2. No matter whichever digit you flip, you will not get the desired string.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   int i,j,t;
+    string a[50];
+    int count=0;
+    cin>>t;
+    for(i=0; i<t; i++)
+    {
+        cin>>a[i];
+        if(a[i][0]== '1')
+        {
+           for(j=1; j<a[i].length(); j++)
+           {
+               if(a[i][j]=='0')
+               {
+                   count++;
+               }
+           }
+        }
+        else if(a[i][0]== '0')
+        {
+           for(j=1; j<a[i].length(); j++)
+           {
+               if(a[i][j]=='1')
+               {
+                   count++;
+               }
+           }
+        }
+        if(count==1 || count == a[i].length()-1 || a[i].length()==1 )
+        {
+            cout<<"Yes"<<endl;
+        }
+        else
+        {
+            cout<<"No"<<endl;
+        }
+        count=0;
+    }
+    
+}
