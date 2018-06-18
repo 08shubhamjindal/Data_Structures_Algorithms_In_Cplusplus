576e727ee2fbec11da3276264bddc433c4fbf5d3
08shubhamjindal <shubhamjindal.cse19@jecrc.ac.in>
2018-06-18 12:26:47 +0530

    Play With Palindrome String

--- Binary-Search-Tree.cpp
+++ Binary-Search-Tree.cpp
@@ -2,75 +2,78 @@
 using namespace std;
 struct Binary
 {
-    int data;
-    Binary* left;
-    Binary* right;
+  int data;
+  Binary *left;
+  Binary *right;
 };
-Binary* newNode(int data)
+Binary *newNode (int data)
 {
-    Binary* node = new Binary();
-    node->data = data;
-    node->left = node->right = NULL;
-    return node;
+  Binary *node = new Binary ();
+  node->data = data;
+  node->left = node->right = NULL;
+  return node;
 }
-Binary* insert(Binary*root , int data)
+
+Binary *insert (Binary * root, int data)
 {
-    if(root==NULL)
+  if (root == NULL)
     {
-        root = newNode(data);
+      root = newNode (data);
     }
-    else if(data <= root->data)
+  else if (data <= root->data)
     {
-        root->left = insert(root->left , data);
+      root->left = insert (root->left, data);
     }
-    else
+  else
     {
-        root->right = insert(root->right , data);
+      root->right = insert (root->right, data);
     }
-    return root;
+  return root;
 }
-bool Search(Binary*root ,int  data)
+
+bool Search (Binary * root, int data)
 {
-    if(root == NULL) 
+  if (root == NULL)
     {
-		return false;
+      return false;
     }
-    else if(root->data = data)
+  else if (root->data = data)
     {
-        return true;
+      return true;
     }
-    else if(data <= root->data)
+  else if (data <= root->data)
     {
-        return Search(root->left , data);
+      return Search (root->left, data);
     }
-    else
+  else
     {
-        return Search(root->right , data);
+      return Search (root->right, data);
     }
 }
-int main()
-{ 
-  int i,a[100];
-  for(i=0; i<5; i++)
-  {
-      cin>>a[i];
-  }
-  Binary* root;
-  root=NULL;
-  for(i=0; i<5; i++)
-   {
-  root = insert(root,a[i]);
-   }
-   int no;
-   cout<<"enter the number be searched"<<endl;
-   cin>>no;
-    if(Search(root,no) == true)
+
+int main ()
+{
+  int i, a[100];
+  for (i = 0; i < 5; i++)
     {
-        cout<<"Found\n";
+      cin >> a[i];
     }
-    else 
+  Binary *root;
+  root = NULL;
+  for (i = 0; i < 5; i++)
     {
-        cout<<"Not Found\n";
+      root = insert (root, a[i]);
+    }
+  int no;
+  cout << "enter the number be searched" << endl;
+  cin >> no;
+  if (Search (root, no) == true)
+    {
+      cout << "Found\n";
+    }
+  else
+    {
+      cout << "Not Found\n";
     }
 //   root = insert(root,15);
 //   root = insert(root,15);
---
+++ String-Palindrome.cpp
@@ -0,0 +1,34 @@
+Monk introduces the concept of palindrome saying,"A palindrome is a sequence of characters which reads the same backward or forward."
+Now, since he loves things to be binary, he asks you to find whether the given string is palindrome or not. If a given string is palindrome, you need to state that it is even palindrome (palindrome with even length) or odd palindrome (palindrome with odd length).
+
+Input:
+The first line consists of T, denoting the number of test cases.
+Next follow T lines, each line consisting of a string of lowercase English alphabets.
+
+Output:
+For each string , you need to find whether it is palindrome or not.
+If it is not a palindrome, print NO.
+If it is a palindrome, print YES followed by a space; then print EVEN it is an even palindrome else print ODD.
+Output for each string should be in a separate line.
+See the sample output for clarification.
+
+Constraints:
+
+
+SAMPLE INPUT
+
+3
+abc
+abba
+aba
+
+SAMPLE OUTPUT
+
+NO
+YES EVEN
+YES ODD
+
+Explanation
+
+The first string is not a palindrome.
+The second and third strings are palindromes of even and odd lengths respectively.
