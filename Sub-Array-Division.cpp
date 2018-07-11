4ee7fa1c7828ec2a1df8363e179b97086fceb9a9
08shubhamjindal <shubhamjindal.cse19@jecrc.ac.in>
2018-07-11 12:22:00 +0530

    Check SubArray and check all element divisible to each other

---
+++ Sub-Array-Division.cpp
@@ -0,0 +1,21 @@
+Here is your task. You are given an array and you have to find the maximum possible length of subarray In that subarray a[i+1]%a[i]==0 Every element in subarray is divisible by its previous element Your task is to find maximum length of subarray.
+INPUT
+first line conatin number of test cases T. Each test case contain number of element in array N.Next line contain elements of array.
+OUTPUT
+print maximum length of subarray
+Constraints
+1<=T<=100 1<=N<=1000000 1<=a[i]<=1000000
+
+SAMPLE INPUT
+
+1
+6
+5 3 10 12 25 20
+
+SAMPLE OUTPUT
+
+3
+
+Explanation
+
+Subarray of maximum length conatins 5 10 20
