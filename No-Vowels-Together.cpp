2a38fea7849a70baa96c0e02e5b0390311a75773
08shubhamjindal <shubhamjindal.cse19@jecrc.ac.in>
2018-07-17 07:47:21 +0530

    Find Combination of String Where No Two vowels Together with or without combination Logic

--- No-Vowels-Together.cpp
+++ No-Vowels-Together.cpp
@@ -1,41 +1,41 @@
-Its Roy's birthday and he is decorating his house with balloons.
-He has 26 different types of balloons. They are represented by [a-z].
+// Its Roy's birthday and he is decorating his house with balloons.
+// He has 26 different types of balloons. They are represented by [a-z].
 
-Now Roy does not want to have few particular balloons together. Actually he doesn't want to keep any of the two vowel (i.e [a,e,i,o,u] ) balloons together.
-So, in one arrangement no two vowels should be together. Given the string of balloons there can be many such arrangements possible.
-Help Roy in finding the number of possible arrangements.
+// Now Roy does not want to have few particular balloons together. Actually he doesn't want to keep any of the two vowel (i.e [a,e,i,o,u] ) balloons together.
+// So, in one arrangement no two vowels should be together. Given the string of balloons there can be many such arrangements possible.
+// Help Roy in finding the number of possible arrangements.
 
-Number of such arrangements can be quite large, so print it modulo 1000000007 (109 + 7)
-Also, there's a chance that no such arrangement is possible. Print "-1" in such cases (without quotes)
+// Number of such arrangements can be quite large, so print it modulo 1000000007 (109 + 7)
+// Also, there's a chance that no such arrangement is possible. Print "-1" in such cases (without quotes)
 
-Input:
-First line contains T, number of test cases.
-Each of next T lines contains a string composed of lower case letters representing all the balloons.
+// Input:
+// First line contains T, number of test cases.
+// Each of next T lines contains a string composed of lower case letters representing all the balloons.
 
-Output:
-For each test case print the required answer.
+// Output:
+// For each test case print the required answer.
 
-Constraints:
-1 <= T <= 100
-1 <= Length of string <= 1000000 (106)
+// Constraints:
+// 1 <= T <= 100
+// 1 <= Length of string <= 1000000 (106)
 
-Note: Sum of string lengths over all the test cases in one file does not exceed 1000000 (106)
-SAMPLE INPUT
+// Note: Sum of string lengths over all the test cases in one file does not exceed 1000000 (106)
+// SAMPLE INPUT
 
-2
-abe
-ae
+// 2
+// abe
+// ae
 
-SAMPLE OUTPUT
+// SAMPLE OUTPUT
 
-2
--1
+// 2
+// -1
 
-Explanation
+// Explanation
 
-Test #1: Possible arrangements are [1] abe [2] eba
+// Test #1: Possible arrangements are [1] abe [2] eba
 
-Test #2: There are no any arrangements possible such that any two vowels are not together. So print -1.
+// Test #2: There are no any arrangements possible such that any two vowels are not together. So print -1.
 
 
 #include<bits/stdc++.h>
@@ -81,6 +81,42 @@ int main()
             //  factorial(cons);
             //  factorial(count);
     cout<<nCrModp(cons+1, count, p)* factorial(cons)*factorial(count)<<endl;
+        
+    }
+    
+}
+-----------------------------------------------------------------------------------------------------------
+One More Way to Find No Vowels Together Without Using Combination
+#include<bits/stdc++.h>
+using namespace std;
+unsigned int factorial(unsigned int n)
+{
+    return (n==1 || n==0) ? 1: n * factorial(n - 1); 
+}
+int main()
+{
+  int t,i,k,j,cons,count=0;
+    cin>>t;
+    int p =1000000009;
+    string s[t+1],s1[t+1];
+    for(i=0; i<t; i++)
+    {
+        cin>>s[i];
+          k=0;
+        for(j=0; j<s[i].length(); j++)
+        {
+            if(s[i][j]=='a' || s[i][j]=='o'|| s[i][j]=='i' || s[i][j]=='e' || s[i][j]=='u' )
+            {
+                count++;
+            }
+        }
+        cons = s[i].length() - count;
+        int x = factorial(cons+1)%p;
+        int y = factorial(count)%p;
+        int z = factorial(cons+1-count)%p;
+        int b = factorial(cons)%p;
+       
+         cout<< ((x/(z*y))%p)*(b%p)*(y%p)<<endl;
         
     }
     
