9f40aa554ca5849b8197f77d5b024fe7f4b2a618
Shubham Jindal <shubhamjindal.cse19@jecrc.ac.in>
2020-01-22 19:16:35 +0530

    Update KadaneWithOperation.cpp

--- KadaneWithOperation.cpp
+++ KadaneWithOperation.cpp
@@ -1,3 +1,9 @@
+Given an array of integers. The task is to find the maximum sum of all the elements of the array after performing the given two operations once each.
+The operations are:
+
+1. Select some(possibly none) continuous elements from the beginning of the array and multiply by -1.
+2. Select some(possibly none) continuous elements from the end of the array and multiply by -1.
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
